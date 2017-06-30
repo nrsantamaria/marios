1d524eb405979615e86d8eb7b50ede740cb64220
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-06-30 13:50:48 -0700

    add scopes and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,19 @@ class Product < ActiveRecord::Base
   validates :cost, :presence => true
   validates :country, :presence => true
   validates :description, :presence => true
+  before_save :upcase_first_letter
 
-  before_save(:upcase_first_letter)
+  scope :mostReviews, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+    )}
+  scope :mostRecent, -> {(
+    order(created_at: :desc).limit(3)
+    )}
+  scope :localProducts, -> { where(country: 'United States')}
 
   define_method(:upcase_first_letter) do
     self.name =  self.name.slice(0,1).capitalize + self.name.slice(1..-1)
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   validates :author, :presence => true
-  validates :content_body, :presence => true
+  validates :content_body, {:presence => true, length: { in: 50..250 }}
   validates :rating, :presence => true
 end
